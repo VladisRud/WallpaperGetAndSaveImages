802ae92b48cbce3bcca63683f1d80bb81835b1fc
Vladis <vladisrud@gmail.com>
2022-05-04 19:47:17 +0300

    Initial Commit

---
+++ ImageGetAndSaveVladisRud/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageGetAndSaveVladisRud
+//
+//  Created by Влад Руденко on 04.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
