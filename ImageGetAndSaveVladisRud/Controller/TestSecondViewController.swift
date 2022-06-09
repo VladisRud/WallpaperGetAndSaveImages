b693891483766e10a28168377be15a1488c7a284
Vladis <vladisrud@gmail.com>
2022-06-09 22:17:43 +0300

    Test TableView in to the ScrollView for the 2-way dimensional scroll

--- ImageGetAndSaveVladisRud/Controller/TestSecondViewController.swift
+++ ImageGetAndSaveVladisRud/Controller/TestSecondViewController.swift
@@ -2,21 +2,28 @@
 //  TestSecondViewController.swift
 //  ImageGetAndSaveVladisRud
 //
-//  Created by Влад Руденко on 08.06.2022.
+//  Created by Влад Руденко on 09.06.2022.
 //
 
 import UIKit
 
 class TestSecondViewController: UIViewController {
 
-    @IBOutlet weak var scrolView: UIScrollView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
